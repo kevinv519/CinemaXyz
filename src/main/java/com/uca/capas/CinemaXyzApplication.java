e9632af5f796abdc3d2b0d4b6a2676fa6d64db4f
Kevin <kevin.vela519@gmail.com>
2019-06-24 21:44:46 -0600

    Proyecto Spring Boot inicializado y configuración básica

---
+++ src/main/java/com/uca/capas/CinemaXyzApplication.java
@@ -0,0 +1,13 @@
+package com.uca.capas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaXyzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaXyzApplication.class, args);
+	}
+
+}
