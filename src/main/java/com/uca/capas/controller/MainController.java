1ebaa34660dbafe8c8ca6f6883c65eebeb0531d8
JorgeAzzuFranco <00074216@uca.edu.sv>
2019-06-26 02:56:17 -0600

    Fixing Git disaster and adding admin and user panel

--- src/main/java/com/uca/capas/controller/MainController.java
+++ src/main/java/com/uca/capas/controller/MainController.java
@@ -1,26 +1,21 @@
 package com.uca.capas.controller;
 
-<<<<<<< HEAD
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-=======
 import java.util.Locale;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
->>>>>>> branch 'master' of https://github.com/kevinv519/CinemaXyz.git
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-<<<<<<< HEAD
 import org.springframework.ui.ModelMap;
-=======
->>>>>>> branch 'master' of https://github.com/kevinv519/CinemaXyz.git
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,7 +30,7 @@ import com.uca.capas.service.FilmService;
 
 @Controller
 public class MainController {
-<<<<<<< HEAD
+
 	
 	@Autowired
 	UserService userService;
@@ -43,25 +38,21 @@ public class MainController {
 	Logger log = Logger.getLogger(MainController.class.getSimpleName());
 	public final static String ATT_LOG = "logedIn";
 	
-=======
-	Logger log = Logger.getLogger(MainController.class.getSimpleName());
-
 	@Autowired
 	private MessageSource messageSource;
 
 	@Autowired
 	FilmService filmService;
 
->>>>>>> branch 'master' of https://github.com/kevinv519/CinemaXyz.git
-	@GetMapping("/")
-<<<<<<< HEAD
+	/*@GetMapping("/")
 	String home(Model model, ModelMap map, HttpSession session) {
 		if (session.getAttribute(ATT_LOG) != null && (boolean)session.getAttribute(ATT_LOG)) {
 			return "redirect:/stores";
 		}
 		model.mergeAttributes(map);
+		return "home";
+	}*/
 		
-=======
 	String home(Model model) {
 		try {
 			model.addAttribute("films", filmService.getAvailableMovies());
@@ -72,7 +63,7 @@ public class MainController {
 			model.addAttribute("message",
 					messageSource.getMessage("home.errorLoading", new Object[] {}, Locale.getDefault()));
 		}
->>>>>>> branch 'master' of https://github.com/kevinv519/CinemaXyz.git
+
 		return "home";
 	}
 	
