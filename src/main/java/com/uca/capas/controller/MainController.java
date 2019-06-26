0f81f9b6478cbce6f1e12bdb4c73361ca5b86202
JorgeAzzuFranco <00074216@uca.edu.sv>
2019-06-26 02:58:06 -0600

    Added control panel for users and admins, login fixed

--- src/main/java/com/uca/capas/controller/MainController.java
+++ src/main/java/com/uca/capas/controller/MainController.java
@@ -44,15 +44,10 @@ public class MainController {
 	@Autowired
 	FilmService filmService;
 
-	/*@GetMapping("/")
-	String home(Model model, ModelMap map, HttpSession session) {
-		if (session.getAttribute(ATT_LOG) != null && (boolean)session.getAttribute(ATT_LOG)) {
-			return "redirect:/stores";
-		}
-		model.mergeAttributes(map);
-		return "home";
+	/*
+	
 	}*/
-		
+	@GetMapping("/")
 	String home(Model model) {
 		try {
 			model.addAttribute("films", filmService.getAvailableMovies());
@@ -66,5 +61,59 @@ public class MainController {
 
 		return "home";
 	}
+	
+	@GetMapping("/login")
+	String home(Model model, ModelMap map, HttpSession session) {
+		if (session.getAttribute(ATT_LOG) != null && (boolean)session.getAttribute(ATT_LOG)) {
+			return "redirect:/stores";
+		}
+		model.mergeAttributes(map);
+		return "login";
+	}
+	
+	@PostMapping("/verify")
+	public String login(@RequestParam String username, @RequestParam String password, RedirectAttributes ra, HttpSession session) {
+		if (username.trim().isEmpty() || password.trim().isEmpty()) {
+			ra.addFlashAttribute("error", "Todos los campos son obligatorios");
+		}
+		else {
+			try {
+				User user = userService.verify(username, password);
+				if (user != null) {
+					session.setAttribute(ATT_LOG, true);
+					if(user.getIsAdmin() == true) {
+						return "redirect:/control-admin";
+					}
+					else {
+						return "redirect:/control-user";
+					}
+				}
+				ra.addFlashAttribute("error", "Usuario o contraseña inválidos");
+			}catch (Exception e) {
+				log.log(Level.SEVERE, "Could not retrieve user", e);
+				ra.addFlashAttribute("error", "No se pudo autenticar el usuario. Intenté más tarde o contacte el administrador del sitio");
+			}
+		}
+		return "redirect:/";
+	}
+	
+	@GetMapping("/logout")
+	String logout(HttpSession session) {
+		session.setAttribute(ATT_LOG, false);
+		return "redirect:/";
+	}
+
+	@GetMapping("/control-admin")
+	String control_adm(){
+		return "control-admin";		
+	}
+	
+	@GetMapping("/control-user")
+	String control_usr(){
+		return "control-user";		
+	}
+	
+	
+	
 	
 }
